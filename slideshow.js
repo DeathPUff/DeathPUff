0b881c29d108936aa5e7cbdbafa119d608b6b393
DeathPUff <91408391+DeathPUff@users.noreply.github.com>
2021-09-29 18:00:06 +0800

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,21 @@
+var control=document.getElementById('control');
+        var opt5=document.getElementById('opt5');
+        var opt4=document.getElementById('opt4');
+        var opt3=document.getElementById('opt3');
+        var opt2=document.getElementById('opt2');
+        var opt1=document.getElementById('opt1');
+        opt5.onmousemove=function(){
+          control.src='./img/205.jpg'
+        }
+        opt4.onmousemove=function(){
+          control.src='./img/204.jpg'
+        }
+        opt3.onmousemove=function(){
+          control.src='./img/203.jpg'
+        }
+        opt2.onmousemove=function(){
+          control.src='./img/202.jpg'
+        }
+        opt1.onmousemove=function(){
+          control.src='./img/201.jpg'
+        }
